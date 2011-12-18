d90754839865f2a86e3d769a536c1a6ae1ce6adb
mutantu2002@gmail.com <mutantu2002@gmail.com@93dbaf12-8ae8-f8e8-12c1-92a8da9bebed>
2011-12-18 08:51:58 +0000

    Add ImageUtils

---
+++ trunk/src/main/java/home/mutant/utils/ImageUtils.java
@@ -0,0 +1,37 @@
+package home.mutant.utils;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class ImageUtils 
+{
+	public static void readImage(String imagePath, float[] inValues) throws IOException 
+	{
+		BufferedImage image = ImageIO.read(new File(imagePath));
+		
+		int offset=0;
+		for (int y = 0; y < image.getHeight(); y++) 
+		{
+			for (int x = 0; x < image.getWidth(); x++) 
+			{
+                int c = image.getRGB(x,y);
+                int  color = (c & 0x00ffffff);
+                
+                inValues[offset]=0;
+                if (color==0)
+                {
+                	inValues[offset]=1;
+                }
+                offset++;
+                if (offset>inValues.length)
+                {
+                	System.out.println("Image too large, truncated, only "+inValues.length+" pixels available");
+                	break;
+                }
+			}
+		}
+	}
+}
