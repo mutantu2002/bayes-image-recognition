f04c0f0b2c09676b0dabdc3364b3f17bbc666133
mutantu2002 <mutantu2002@gmail.com>
2014-09-09 15:46:49 +0000

    Add training main that treats every pixel as a feature

--- src/main/java/home/mutant/trainings/TrainMnistNaiveBayes.java
+++ src/main/java/home/mutant/trainings/TrainMnistNaiveBayes.java
@@ -1,19 +1,46 @@
 package home.mutant.trainings;
 
 import home.mutant.bayes.NaiveBayes;
+import home.mutant.deep.ui.Image;
+import home.mutant.deep.utils.ImageUtils;
+import home.mutant.deep.utils.ImageUtils.Style;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TrainMnistNaiveBayes
 {
-	private NaiveBayes bayes = new NaiveBayes();
+	List<Image> trainImages = new ArrayList<Image>();
+	List<Integer> trainLabels  = new ArrayList<Integer>();
+
+	List<Image> testImages = new ArrayList<Image>();
+	List<Integer> testLabels  = new ArrayList<Integer>();
+	public Style style=Style.BW;
+	
+	private List<NaiveBayes> bayes = new ArrayList<NaiveBayes>();
 	
 	public static void main(String[] args) throws Exception
 	{
 		new TrainMnistNaiveBayes().train();
 	}
 
-	private void train()
+	private void train() throws Exception
 	{
-		// TODO Auto-generated method stub
-		
+		for (int i=0;i<10;i++)
+		{
+			bayes.add(new NaiveBayes());
+		}
+
+		ImageUtils.loadImages(trainImages, testImages, trainLabels, testLabels, style);
+		for (int index = 0; index<10000; index++)
+		{
+			NaiveBayes currentBayes = bayes.get(trainLabels.get(index));
+			int feature = 0;
+			byte[] oneDimension = trainImages.get(index).getDataOneDimensional();
+			for (int i = 0; i < oneDimension.length; i++)
+			{
+				
+			}
+		}
 	}
 }
