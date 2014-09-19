6c4e8fa46dacfaf85a5f87b0be127930d2ad5a10
mutantu2002@gmail.com <mutantu2002@gmail.com@93dbaf12-8ae8-f8e8-12c1-92a8da9bebed>
2014-09-19 12:45:12 +0000

    Generalize mnistr training class

---
+++ trunk/src/main/java/home/mutant/trainings/TrainMnist1Pixel.java
@@ -0,0 +1,32 @@
+package home.mutant.trainings;
+
+import home.mutant.deep.ui.Image;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TrainMnist1Pixel extends TrainMnist
+{
+	public static void main(String[] args) throws Exception
+	{
+		new TrainMnist1Pixel().train();
+	}
+
+	public List<Integer> getFeatures(Image image)
+	{
+		List<Integer> features = new ArrayList<Integer>();
+		byte[] oneDimension = image.getDataOneDimensional();
+		for (int i = 0; i < oneDimension.length; i++)
+		{
+			if (oneDimension[i]!=0)
+			{
+				features.add(i);
+			}
+			else
+			{
+				features.add(-1*i);
+			}
+		}
+		return features;
+	}
+}
