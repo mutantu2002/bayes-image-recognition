2d8f1d937e81799d16ccbdb8e2e4c9fa427bc3ed
mutantu2002@gmail.com <mutantu2002@gmail.com@93dbaf12-8ae8-f8e8-12c1-92a8da9bebed>
2014-09-19 07:46:36 +0000

    Generalize mnistr training class

---
+++ trunk/src/main/java/home/mutant/trainings/TrainMnist2Pixels.java
@@ -0,0 +1,17 @@
+package home.mutant.trainings;
+
+import home.mutant.deep.ui.Image;
+
+import java.util.List;
+
+public class TrainMnist2Pixels extends TrainMnist
+{
+
+	@Override
+	public List<Integer> getFeatures(Image image)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
