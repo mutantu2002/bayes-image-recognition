54c989c3dad6db657c7205bab69f4982fe996794
mutantu2002 <mutantu2002@gmail.com>
2014-09-23 11:55:34 +0000

    Segments constant

--- src/main/java/home/mutant/trainings/multi/FeaturableSegments.java
+++ src/main/java/home/mutant/trainings/multi/FeaturableSegments.java
@@ -8,19 +8,20 @@ import java.util.List;
 
 public class FeaturableSegments implements  Featurable
 {
-	static final int SEGMENTS =10;
+	static final int SEGMENTS =100;
+	static final int STEP_SEGMENTS = 1;
 	@Override
 	public List<Integer> getFeatures(Image image)
 	{
 		List<Integer> features = new ArrayList<Integer>();
 		byte[] oneDimension = image.getDataOneDimensional();
-		for (int i = 0; i < oneDimension.length-3*SEGMENTS; i++)
+		for (int i = 0; i < oneDimension.length-STEP_SEGMENTS*SEGMENTS; i++)
 		{
 			if (oneDimension[i]!=0)
 			{
 				for (int j = 0; j<=SEGMENTS; j+=1)
 				{
-					if (oneDimension[i+3*j]!=0)
+					if (oneDimension[i+STEP_SEGMENTS*j]!=0)
 					{
 						features.add(oneDimension.length*i+j);
 					}
