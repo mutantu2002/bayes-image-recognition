5f23d5b4b96ba237f5768861c9afac2f92a8636f
mutantu2002 <mutantu2002@gmail.com>
2014-10-07 15:12:52 +0000

    Load&show image rudiments

---
+++ src/main/java/home/mutant/view/ShowRudiments.java
@@ -0,0 +1,27 @@
+package home.mutant.view;
+
+
+import home.mutant.deep.ui.Image;
+import home.mutant.deep.ui.ResultFrame;
+import home.mutant.utils.ImageUtils;
+
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.imageio.ImageIO;
+
+public class ShowRudiments
+{
+	public static void main(String[] args) throws Exception
+	{
+		ResultFrame frame = new ResultFrame(1200, 600);
+		BufferedImage resImage = ImageIO.read(ShowRudiments.class.getResourceAsStream("/rudiment1.bmp"));
+		List<Image> list = new ArrayList<Image>();
+		for (double theta = 0; theta<2*Math.PI;theta+=Math.PI/20)
+		{
+			list.add(new Image(ImageUtils.rotate(resImage, theta)));
+		}
+		frame.showImages(list);
+	}
+}
