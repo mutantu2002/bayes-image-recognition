d5a879b818a77e159f1c92fd746824b64d5ec4dc
mutantu2002 <mutantu2002@gmail.com>
2015-02-25 09:50:09 +0000

    Add test cases

--- src/test/java/home/mutant/bayes/NeuronBayesTest.java
+++ src/test/java/home/mutant/bayes/NeuronBayesTest.java
@@ -10,25 +10,31 @@ public class NeuronBayesTest
 		NeuronBayes neuron = new NeuronBayes(25);
 		byte[] pixelsLeft = new byte[25];
 		byte[] pixelsRight = new byte[25];
+		byte[] pixelsAll = new byte[25];
 		for (int i=0;i<5;i++)
 		{
 			pixelsLeft[i*5] = (byte)255;
 			pixelsRight[i*5+4] = (byte)255;
 		}
-		
+		for (int i=0;i<25;i++)
+		{
+			pixelsAll[i] = (byte)255;
+		}
+		neuron.modifyWeights(new byte[25]);
 		neuron.modifyWeights(pixelsLeft);
 		neuron.modifyWeights(pixelsLeft);
+		neuron.modifyWeights(pixelsRight);
+		neuron.modifyWeights(pixelsAll);
 		neuron.modifyWeights(pixelsRight);
 		neuron.modifyWeights(pixelsLeft);
 		neuron.modifyWeights(new byte[25]);
 		
-		for (int i=0;i<5;i++)
-		{
-			pixelsLeft[i*5] = (byte)254;
-			pixelsRight[i*5+4] = (byte)254;
-		}
+		pixelsLeft[0]=0;
 		neuron.modifyWeights(pixelsLeft);
-		neuron.modifyWeights(pixelsRight);		
+		pixelsLeft[5]=0;
+		neuron.modifyWeights(pixelsLeft);
+		pixelsLeft[10]=0;
+		neuron.modifyWeights(pixelsLeft);
 		System.out.println();
 	}
 }
